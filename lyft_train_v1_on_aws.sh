893d46866e1349d4f1dbe418d1078c0698e0f262
kanhua <kanhua.lee@gmail.com>
2019-11-22 22:25:00 +0900

    debug lyft_train_v1_on_aws.sh

--- lyft_train_v1_on_aws.sh
+++ lyft_train_v1_on_aws.sh
@@ -3,10 +3,10 @@ MODEL_LOG_DIR="/dltraining/log_v1"
 RESTORE_MODEL_PATH="/dltraining/log_v1/model.ckpt"
 for VALUE in {0..15}
 do
-  data_file=/dltraining/artifacts/lyft_frustum_$VALUE.pickle
+  data_file='/dltraining/artifacts/lyft_frustum_'$VALUE'.pickle'
   echo $data_file
   python train.py --gpu 0 --model frustum_pointnets_v1 --log_dir $MODEL_LOG_DIR --num_point 1024 \
                   --max_epoch 5 --batch_size 32 --decay_step 800000 \
-                  --decay_rate 0.5 --data_dir $DATA_DIR --restore_model_path $RESTORE_MODEL_PATH
+                  --decay_rate 0.5 --data_dir $data_file --restore_model_path $RESTORE_MODEL_PATH
 done
 
