ee0b372775f35deb17061ecc1c73a29fd08c2ac0
kanhua <kanhua.lee@gmail.com>
2019-11-22 11:59:47 +0900

    add AWS linking scripts

---
+++ link_file_on_aws.sh
@@ -0,0 +1,5 @@
+DATA_PATH=/dltraining/dataset
+ln -s $DATA_PATH/train_images $DATA_PATH/images
+ln -s $DATA_PATH/train_data $DATA_PATH/data
+ln -s $DATA_PATH/train_maps $DATA_PATH/maps
+ln -s $DATA_PATH/train_lidar $DATA_PATH/lidar
