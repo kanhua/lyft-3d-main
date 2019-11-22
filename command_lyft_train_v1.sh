6756b0a607b12939de87ea590518b50693a60bd4
kanhua <kanhua.lee@gmail.com>
2019-11-22 12:27:43 +0900

    update script names

--- command_lyft_train_v1.sh
+++
@@ -1,3 +0,0 @@
-DATA_DIR="/dltraining/artifacts/lyft_val_4.pickle"
-MODEL_LOG_DIR="/dltraining/log_v1"
-python train.py --gpu 0 --model frustum_pointnets_v1 --log_dir $MODEL_LOG_DIR --num_point 1024 --max_epoch 5 --batch_size 32 --decay_step 800000 --decay_rate 0.5 --data_dir $DATA_DIR
