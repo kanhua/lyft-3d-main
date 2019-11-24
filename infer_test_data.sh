3893189f7f5108b0f746cac0be328284c9913346
kanhua <kanhua.lee@gmail.com>
2019-11-24 11:19:47 +0900

    bug: corrected wrong path of model checkpoints in evaluation scripts

--- infer_test_data.sh
+++ infer_test_data.sh
@@ -2,7 +2,7 @@
 python3 test_prepare_data_from_rgb_detection.py
 python3 /Users/kanhua/Downloads/frustum-pointnets/train/test.py \
         --data_path /Users/kanhua/Dropbox/Programming/lyft-3d-main/artifact/lyft_val_from_rgb.pickle \
-        --model_path /Users/kanhua/Downloads/frustum-pointnets/train/log_v1/model.ckpt \
+        --model_path ./log_v1/model.ckpt \
         --batch_size 32 \
         --dump_result \
         --from_rgb_detection \
--- infer_train_eval_data.sh
+++ infer_train_eval_data.sh
@@ -2,7 +2,7 @@
 DATA_ID=17
 python3 /Users/kanhua/Downloads/frustum-pointnets/train/test.py \
         --data_path /Users/kanhua/Downloads/3d-object-detection-for-autonomous-vehicles/artifacts/lyft_frustum_$DATA_ID.pickle \
-        --model_path /Users/kanhua/Downloads/frustum-pointnets/train/log_v1/model.ckpt \
+        --model_path ./log_v1/model.ckpt \
         --batch_size 32 \
         --dump_result \
         --output /Users/kanhua/Downloads/lyft-3d-results
