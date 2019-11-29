74e72beab629a54ca1efb32620a72a37c4dbfc75
kanhua <kanhua.lee@gmail.com>
2019-11-29 20:02:39 +0900

    update prepare_lyft_data.py

--- prepare_lyft_data.py
+++ prepare_lyft_data.py
@@ -1176,11 +1176,11 @@ if __name__ == "__main__":
     total_scenes = 181
     batch = 10
     batch_num = int(total_scenes / batch)
-    for idx in range(1):
+    for idx in range(18):
         opt_file_pat = "lyft_frustum_{}.pickle".format(idx)
         token_file_pat = "lyft_frustum_token_{}.pickle".format(idx)
         output_file = os.path.join(ARTIFACT_PATH, opt_file_pat)
         token_file = os.path.join(ARTIFACT_PATH, token_file_pat)
         # prepare_frustum_data_from_traincsv(64, output_file)
-        prepare_frustum_data_from_scenes(36, output_file, lyftdf=level5data, token_filename=token_file,
+        prepare_frustum_data_from_scenes(1000000, output_file, lyftdf=level5data, token_filename=token_file,
                                          scenes=range(idx * batch, (idx + 1) * batch))
