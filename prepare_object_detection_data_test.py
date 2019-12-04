a5e26b8e4bbf2c5a12171197675ede8cfc989400
kanhua <kanhua.lee@gmail.com>
2019-12-04 15:01:46 +0900

    update test code

--- prepare_object_detection_data_test.py
+++ prepare_object_detection_data_test.py
@@ -1,12 +1,11 @@
 from prepare_lyft_data import get_paths
 import pandas as pd
 import os
+import numpy as np
 
 from prepare_object_detection_data import select_annotation_boxes,level5data,create_tf_feature,parse_protobuf_message
 
 def test_run_through_sample_token():
-
-
     DATA_PATH, ARTIFACT_PATH, _ = get_paths()
 
     first_sample_token = '24b0962e44420e6322de3f25d9e4e5cc3c7a348ec00bfa69db21517e4ca92cc8'  # this is for test
@@ -22,9 +21,6 @@ def test_run_through_sample_token():
             print(c)
 
 def test_write_and_read():
-    from prepare_lyft_data import get_paths
-    import pandas as pd
-    import os
     from skimage.io import imread
     from vis_util import draw_bounding_boxes_on_image_array
     import matplotlib.pyplot as plt
@@ -37,15 +33,13 @@ def test_write_and_read():
 
     df = pd.read_csv(default_train_file)
 
-    image_dir = "/Users/kanhua/Downloads/3d-object-detection-for-autonomous-vehicles/images"
-
     for image_filepath, cam_token, corners, boxes, img_width, img_height in select_annotation_boxes(first_sample_token,
                                                                                                     level5data):
         tf_example = create_tf_feature(image_file_path=image_filepath, camera_token=cam_token,
                                        corner_list=corners, image_width=img_width, image_height=img_height, boxes=boxes)
         example_message = tf_example.SerializeToString()
         filename, xmin, xmax, ymin, ymax = parse_protobuf_message(example_message)
-        image_array = imread(os.path.join(image_dir, filename))
+        image_array = imread(filename)
 
         box = np.vstack([ymin, xmin, ymax, xmax])
         box = np.transpose(box)
