d18c7b0967bfec8da2b4e8c6db50e5567c089fb4
kanhua <kanhua.lee@gmail.com>
2019-12-07 22:25:03 +0900

    config tool bug fix

--- test_prepare_data_from_rgb_detection.py
+++ test_prepare_data_from_rgb_detection.py
@@ -1,8 +1,8 @@
 import os
 from prepare_lyft_data import prepare_frustum_data_from_scenes, level5data
-from test_data_loader import level5testdata
-output_file = os.path.join("./artifact/lyft_val_from_rgb.pickle")
-token_file = os.path.join("./artifact/lyft_val_token_from_rgb.pickle")
+#from test_data_loader import level5testdata
+output_file = os.path.join("/dltraining/artifacts/lyft_val_from_rgb.pickle")
+token_file = os.path.join("/dltraining/artifacts/lyft_val_token_from_rgb.pickle")
 # prepare_frustum_data_from_traincsv(64, output_file)
 prepare_frustum_data_from_scenes(20000000, output_file, token_filename=token_file, scenes=range(151,160),
                                  from_rgb_detection=True, lyftdf=level5data)
