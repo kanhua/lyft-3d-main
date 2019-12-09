da46e47031320e9542ba0773889b78918a56b7ae
kanhua <kanhua.lee@gmail.com>
2019-12-09 14:43:33 +0900

    lab: add tf v2 trial scripts

--- tf_lab/test_tf_dataset.py
+++ tf_lab/test_tf_dataset.py
@@ -1,17 +1,20 @@
 import tensorflow as tf
 from itertools import count
 
-dataset = tf.data.Dataset.from_tensor_slices([8, 3, 0, 8, 2, 1])
 
-sess = tf.Session()
+g=tf.Graph()
+with g.as_default():
+    dataset = tf.data.Dataset.from_tensor_slices([8, 3, 0, 8, 2, 1])
+    iter = dataset.make_one_shot_iterator()
+    el = iter.get_next()
+    el = iter.get_next()
+    el2=tf.compat.v1.add(el,el)
 
-iter = dataset.make_one_shot_iterator()
-el = iter.get_next()
-
-try:
-    for x in count():
-        data_numpy=sess.run(el)
-        print(data_numpy)
-except tf.errors.OutOfRangeError:
-        print("Process finished")
-        pass
+    with tf.compat.v1.Session() as sess:
+        try:
+            for x in count():
+                data_numpy=sess.run(el2)
+                print(data_numpy)
+        except tf.errors.OutOfRangeError:
+                print("Process finished")
+                pass
