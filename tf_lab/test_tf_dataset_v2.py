80fa5725ca8263f0a6e815c7768679de28b6d6f3
kanhua <kanhua.lee@gmail.com>
2019-12-09 14:43:33 +0900

    lab: add tf v2 trial scripts

--- tf_lab/test_tf_dataset_v2.py
+++ tf_lab/test_tf_dataset_v2.py
@@ -5,7 +5,7 @@ from itertools import count
 g=tf.Graph()
 with g.as_default():
     dataset = tf.data.Dataset.from_tensor_slices([8, 3, 0, 8, 2, 1])
-    iter = dataset.make_one_shot_iterator()
+    iter = tf.compat.v1.data.make_one_shot_iterator(dataset)
     el = iter.get_next()
     el = iter.get_next()
     el2=tf.compat.v1.add(el,el)
