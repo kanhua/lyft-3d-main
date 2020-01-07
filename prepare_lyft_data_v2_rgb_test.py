865aa0cf8ad11f9b9c4f704d531d01a801f7ff0e
kanhua <kanhua.lee@gmail.com>
2020-01-07 22:00:43 +0900

    test: improved unit tests in prepare_lyft_data_v2_rgb_test.py

--- prepare_lyft_data_v2_rgb_test.py
+++ prepare_lyft_data_v2_rgb_test.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 tf.compat.v1.enable_eager_execution()
 from prepare_lyft_data_v2 import FrustumGenerator, get_all_boxes_in_single_scene, \
-    parse_frustum_point_record, load_train_data, FrustumPoints2D,parse_frustum_point_record_2d
+    parse_frustum_point_record, load_train_data, FrustumPoints2D, parse_frustum_point_record_2d
 import matplotlib.pyplot as plt
 import numpy as np
 from test_data_loader import load_test_data
@@ -13,16 +13,18 @@ from object_classifier import TLClassifier
 class FrustumRGBTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.level5testdata = load_test_data()
-        #self.object_classifier = TLClassifier()
+        self.object_classifier = TLClassifier()
 
     def test_write_frustum_object(self):
-        fp2 = FrustumPoints2D(lyftd=self.level5testdata, point_cloud_in_box=np.zeros((5, 3),dtype=np.float), box_2d_pts=np.array([1, 2, 3, 4]),
+        dummy_pc = np.zeros((5, 3), dtype=np.float32)
+        fp2 = FrustumPoints2D(lyftd=self.level5testdata, point_cloud_in_box=dummy_pc, box_2d_pts=np.array([1, 2, 3, 4]),
                               frustum_angle=0.2, sample_token='sample_token',
                               camera_token='cam_token', score=1.0, object_name='car')
 
         example = fp2.to_train_example()
         example_proto_str = example.SerializeToString()
         example_tensors = parse_frustum_point_record_2d(example_proto_str)
+        self.assertTrue(np.allclose(example_tensors['frustum_point_cloud'].numpy(), fp2.point_cloud_in_box))
 
     def test_one_sample_token(self):
         test_sample_token = self.level5testdata.sample[0]['token']
