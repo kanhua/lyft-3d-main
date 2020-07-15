828672f2b8981d48b48c4f8f2668601a5eff64f8
kanhua <kanhua.lee@gmail.com>
2020-07-15 09:46:54 +0900

    fea: update score calculation

--- test_score_calculator.py
+++ test_score_calculator.py
@@ -3,6 +3,7 @@ import pandas as pd
 from test_making_inference import ScoreCalculator
 from config_tool import get_paths
 import os
+import numpy as np
 
 data_path, _, _ = get_paths()
 
@@ -16,10 +17,11 @@ scores = sc.calculate_single_entry(1)
 
 mean_score = sc.calculate_mean_ious()
 
-print(scores)
+#print(scores)
+#print(mean_score)
 
-print(mean_score)
+iou_levels=[0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
 
-avg_precision = sc.calculate_average_precision(0.5)
-
-print("avg precision: {}".format(avg_precision))
+for il in iou_levels:
+    avg_precision = sc.calculate_average_precision(il)
+    print("iou level:{}, avg precision: {}".format(il, np.mean(avg_precision)))
